c1f6206b2ec0d430db4ea0356cf8a627ce4044d4
Surya Naga Sesank M <67413218+nagasesank@users.noreply.github.com>
2026-07-18 13:15:55 +0530

    Update user_data.sh

--- aws/modules/compute/user_data.sh
+++ aws/modules/compute/user_data.sh
@@ -12,7 +12,7 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
 -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
-http://169.254.169.254/latest/meta-data/instance-id)
+http://<ur-ip>/latest/meta-data/instance-id)
 
 HOSTNAME=$(hostname)
 
